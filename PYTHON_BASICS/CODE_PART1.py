38517c00b50744d0400bc837472f59e953bd29a1
Dhruba Jyoti Kalita <51885093+dhruba18@users.noreply.github.com>
2021-04-24 13:17:37 +0530

    Update CODE_PART1.py

--- PYTHON_BASICS/CODE_PART1.py
+++ PYTHON_BASICS/CODE_PART1.py
@@ -8,15 +8,24 @@ for language in programming_languages:
     print(language)
 
 # ------ List -------------------
-l = [1,2,3,4,5]
+l = [1, 2, 3, 4, 5]
 print("The element to be print:")
 print(l[1])
 print(l[-1])
 print(l[1:3])
 print(l[2:])
 print(l)
-l[1]= 99
+l[1] = 99
 print(l)
+
+
+def addition(x, y):
+    return x + y
+
+
+z = addition(5, 3)
+print(" The result of the additon is", z)
+print(addition("Nutan", "Nagar"))
 
 # ----------- Tic-Tac-Toe ------------------
 game = [[0, 0, 0],
@@ -31,6 +40,7 @@ for row in game:
     print(count1, row)
     count1 = count1 + 1
 
+
 # Python Built in function
 def game_board():
     print("   0  1  2")
@@ -39,7 +49,21 @@ def game_board():
             print(item)'''
         print(count2, row)
 
-x = game_board
-game[0][1] = 1
-x()
 
+'''x = game_board
+game[0][1] = 1
+x()'''
+'''game_board()
+game[0][1] = 1
+game_board()'''
+
+
+def game_board1(player=0, row=0, column=0 , just_display = False):
+    print("   0  1  2")
+    if not just_display:
+         game[row][column] = player
+    for count2, row in enumerate(game):
+        print(count2, row)
+
+game_board1(just_display)
+game_board1(player=1, row=2, column=0)
